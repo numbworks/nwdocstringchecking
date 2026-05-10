991b25a76e884b04c731901c2b86e9bc7f256bd0
numbworks <numbworks@gmail.com>
2026-05-10 12:07:18 +0200

    #2 setupinfo.py added.

---
+++ src/setupinfo.py
@@ -0,0 +1,14 @@
+'''Contains project information.'''
+
+# INFORMATION
+PROJECT_VERSION : str = "2.0.0"
+PROJECT_AUTHOR : str = "numbworks"
+PROJECT_ALIAS : str = "nwds"
+
+LIBRARY_NAME : str = "nwdocstringchecking"
+LIBRARY_DESCRIPTION : str = "A library designed to identify which methods in a Python file are missing docstrings."
+
+CLI_NAME : str = "nwdocstringcheckingcli"
+CLI_DESCRIPTION : str = "A CLI application designed to identify which methods in a Python file are missing docstrings."
+
+PROJECT_URL : str = f"https://github.com/{PROJECT_AUTHOR}/{LIBRARY_NAME}"
